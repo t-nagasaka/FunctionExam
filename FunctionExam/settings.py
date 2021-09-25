fb3ac5ffbae54868e343fa29eec45066e08fd3be
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-09-25 22:50:08 +0900

    fix settings

--- FunctionExam/settings.py
+++ FunctionExam/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'FunctionExam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
