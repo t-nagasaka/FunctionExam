ecf882055c13d791831bbf02dd39d03fa49d4736
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-09-25 23:53:38 +0900

    fix settings

--- FunctionExam/settings.py
+++ FunctionExam/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = ''
 
 TIME_ZONE = 'Asia/Tokyo'
 
