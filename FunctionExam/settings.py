0d7b2ac937c608a03be9115bfd057d3d76862f7f
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-09-25 23:02:36 +0900

    fix settings

--- FunctionExam/settings.py
+++ FunctionExam/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR.joinpath('templates')
+STATIC_DIR = BASE_DIR.joinpath('static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
