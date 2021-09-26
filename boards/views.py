b68bf1708937c6d41b6a3a2cd0bea299f816b6eb
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-09-27 07:02:25 +0900

    done doard_edit

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,6 @@
+from django.http.response import Http404
 from .models import Themes
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from . import forms
 from django.contrib import messages
 
@@ -25,3 +26,18 @@ def list_themes(request):
             'themes': themes
         }
     )
+
+
+def edit_theme(request, id):
+    theme = get_object_or_404(Themes, id=id)
+    if theme.user.id != request.user.id:
+        raise Http404
+    edit_theme_form = forms.CreateThemeForm(request.POST or None, instance=theme)
+    if edit_theme_form.is_valid():
+        edit_theme_form.save()
+        messages.success(request, '掲示板を更新しました')
+        return redirect('boards:list_themes')
+    return render(request, 'boards/edit_theme.html', context={
+        'edit_theme_form': edit_theme_form,
+        'id': id
+    })
