d0c0ecca6e9fdf5b5c54267a6d4e6e14d11533db
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-09-26 22:10:01 +0900

    done is_active

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, PermissionsMixin)
+from django.db.models.signals import post_save
+from  django.dispatch import receiver
+from uuid import uuid4
+from datetime import datetime, timedelta
 
 
 class Users(AbstractBaseUser, PermissionsMixin):
@@ -16,3 +20,40 @@ class Users(AbstractBaseUser, PermissionsMixin):
 
     class Meta:
         db_table = 'users'
+
+
+class UserActivateTokensManager(models.Manager):
+
+    def activate_user_by_token(self, token):
+        user_activate_token = self.filter(
+            token=token,
+            expired_at__gte=datetime.now()
+        ).first()
+        user = user_activate_token.user
+        user.is_active = True
+        user.save()
+
+
+class UserActivateTokens(models.Model):
+    token = models.UUIDField(db_index=True)
+    expired_at = models.DateTimeField()
+    user = models.ForeignKey(
+        'Users', on_delete=models.CASCADE
+    )
+
+    objects = UserActivateTokensManager()
+
+    class Meta:
+        db_table = 'user_activate_token'
+
+
+@receiver(post_save, sender=Users)
+def publish_token(sender, instance, **kwargs):
+    print(str(uuid4))
+    print(datetime.now() + timedelta(days=1))
+    user_activate_token = UserActivateTokens.objects.create(
+        user=instance, token=str(uuid4()),
+        expired_at=datetime.now() + timedelta(days=1)
+    )
+    print((f'''http://127.0.0.1:8000/accounts/activate_user/
+           {user_activate_token.token})'''))
--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('', views.home, name='home'),
     path('regist', views.regist, name='regist'),
+    path('activate_user/<uuid:token>', views.activate_user, name='activate_user')
 ]
